6768d0ee07b3c077dd5b81ced3070b55fd3846f6
Miguel Pires <miguelpires94@gmail.com>
2014-04-07 19:30:34 +0100

    Added recursion for directories
    Identifies directories and the files within them

--- src/polyglot.py
+++ src/polyglot.py
@@ -6,22 +6,26 @@ languages = {"c" : "C", "py": "Python"}
 def cutFileName (fileName):
 	return fileName.rfind('.')+1
 
-fileName = sys.argv[1]
+def polyglot (fileName):
 
-if fileName[0] == '\'' and fileName[len(fileName)-1] == '\'':
-	fileName = fileName[1:len(fileName)-1] 
+	if fileName[0] == '\'' and fileName[len(fileName)-1] == '\'':
+		fileName = fileName[1:len(fileName)-1] 
 
-filePath = os.path.abspath(fileName)
+	filePath = os.path.abspath(fileName)
 
+	if not os.path.exists (filePath):
+		print "File doesn't exist"
+		sys.exit()	
 
-if not os.path.exists (filePath):
-	print "File doesn't exist"
-	sys.exit()	
+	if os.path.isfile (filePath):
+		extensionIndex = cutFileName (fileName)
+		extension = fileName [extensionIndex :]
+		print languages [extension], "file"
 
-if os.path.isfile (filePath):
-	extensionIndex = cutFileName (fileName)
-	extension = fileName [extensionIndex :]
-	print languages [extension]
+	elif os.path.isdir (filePath):
+		print "In directory:", fileName
+		contentList = os.listdir (filePath)
+		for file in contentList:
+			polyglot (os.path.join(filePath, file))
 
-elif os.path.isdir (filePath):
-	print "Argument is a directory"
+polyglot (sys.argv[1])
