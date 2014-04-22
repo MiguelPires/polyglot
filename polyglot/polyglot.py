e82e573731bb060a61da07d38deb2eecfb4aa72e
Miguel Pires <miguelpires94@gmail.com>
2014-04-22 21:58:20 +0100

    Changed some variables' storage location
    The language counters and the list containing unknown extensions are now
    internal instead of being in the YAML file. Also added some
    documentation, in form of both comments and a more descriptive
    docstring. Deleted some unused methods. Also the current directory can
    now be passed as an argument to Polyglot

--- polyglot/polyglot.py
+++ polyglot/polyglot.py
@@ -2,116 +2,107 @@ from __future__ import division
 import os, yaml
 
 class Polyglot:
-	""" Polyglot searches for the file passed as an argument in the directories below the user's CWD.
-	It does the same for the languages file."""
+	""" Polyglot searches for the file passed as an argument in or below the user's CWD.
+	It does the same for the languages file. The file extensions are searched in a .yml file and statistics pertaining to
+	the occurrences of known or unknown extensions are outputed"""
 
-	def __init__(self, fileName, languagesFileArg = 'languages.yml'):
+	def __init__ (self, fileName, languagesFileArg = 'languages.yml'):
 		self.baseFile = fileName
 		self.languagesFile = self.tryOpenFile (languagesFileArg)
 		self.languages = yaml.safe_load (self.languagesFile)
 		self.totalFilesCounter = 0
 		self.recognizedFilesCounter = 0
+		self.languagesCounter = {}
+		self.unknownLanguagesCounter = 0
+		self.unknownLanguages = []
 		self.runAnalysis (fileName)
 
 	def __repr__ (self):
 		if not self.totalFilesCounter == 0:
-			return self.knownLanguagesString() + self.unknownLanguagesString() + "\n" + self.ignoredLanguagesString()
+			return self.knownLanguagesString() + self.unknownLanguagesString()				# the class representation - the analysis stats
 
 		else:
 			return "No files read."
 
-	def __del__ (self): 
-		"""with open ('output.yaml', 'w') as output:	
-			output.write (yaml.dump (self.languages, output, default_flow_style=True))
-		self.languagesFile.close()""" #poop happening
-
-	@staticmethod
-	def tryOpenFile (file):
-		try:
-			return open(Polyglot.find (file) , 'r+')
-		except IOError, e:
-		 	print e
-
-	@classmethod
-	def find (cls, name, root = os.getcwd()):
-		for path, dirs, files in os.walk(root, False):
-			if name in files or name in dirs:
-				return os.path.join(path, name)
+	#def __del__ (self): 
+		#self.languagesFile.close() #poop happening
+	
+	#*** String methods for printing output. Prints the percentages of each recognized file type, total recognized files, unknown extension, etc ***
 
 	def knownLanguagesString (self):
 		v = str (round(self.recognizedFilesCounter / self.totalFilesCounter*100, 1))+ "% of the files were recognized. Recognized files:\n"
-		for key, value in self.languages.iteritems():
-			languagecounter = value[1]
-			if not languagecounter == 0 and key != 'Ignore' and key != 'Unknown':
-				v += "** " + str (key)+ ": " + str (round(languagecounter / self.totalFilesCounter*100, 1)) + "%\n"
+		for key, counter in self.languagesCounter.iteritems():
+				v += "** " + str (key)+ ": " + str (round(counter / self.totalFilesCounter*100, 1)) + "%\n"
 		
 		return v
 
 	def unknownLanguagesString (self):
-		unknownLanguagesCounter = self.languages ['Unknown'][1]
-		if not unknownLanguagesCounter == 0:
-			return str (round(unknownLanguagesCounter / self.totalFilesCounter*100, 1))+ "% of the files were of unknown type. Unknown extensions:\n" + str (self.languages ['Unknown'][0]).strip('[]')
+		if not self.unknownLanguagesCounter == 0:
+			return str (round(self.unknownLanguagesCounter / self.totalFilesCounter*100, 1))+ "% of the files were of unknown type. Unknown extensions:\n" + str (self.unknownLanguages).strip('[]')
 
 		else:
 			return ""
 
-	def ignoredLanguagesString(self):
-		ignoredLanguagesCounter = self.languages ['Ignore'][1]
-		if not ignoredLanguagesCounter == 0:
-			return str (round(ignoredLanguagesCounter / self.totalFilesCounter*100, 1))+ "% of the files were recognized but ignored. Ignored extensions:\n" + str (self.languages ['Ignore'][0]).strip('[]')
-		else:
-			return ""
+	#*** Methods related to finding files below or in the current directory; used for finding and opening the YAML file ****
+
+	@staticmethod
+	def tryOpenFile (file):
+		try:
+			return open(Polyglot.find (file) , 'r')								# the corresponding .close() isn't coded yet 
+		except IOError, e:
+			print e
+
+	@classmethod
+	def find (cls, name, root = os.path.dirname (os.getcwd())):					# searches for the file in the current directory tree
+		for path, dirs, files in os.walk(root, False):
+			if name in files or name in dirs:
+				return os.path.join(path, name)
+
+		errorMsg = "File\\directory "+name+"was not found."						# 404: file not found
+		raise Exception (errorMsg)
+
 
 	@staticmethod
 	def parseFileName (fileName):
 		if fileName[0] == '\'' and fileName[len(fileName)-1] == '\'':
-			fileName = fileName[1:len(fileName)-1] 
+			fileName = fileName[1:len(fileName)-1]
 
 		return Polyglot.find (fileName)
 
-	def generateExtensions (self):											# unused but pretty cool									
-		for sublist in self.languages.values():
-			for item in sublist:
-				yield item
 
-	def generateSublist (self):											# unused but pretty cool									
-		for sublist in self.languages.values():
-			yield sublist
-
-	def getListFromExtension (self, extension):						# returns the of data of the language where 'extension' belongs
-		for sublist in self.languages.values():						# if extension isn't found, returns None
-			if extension in sublist[0]:								# sublist = [extensionsList, counter]
+	def getListFromExtension (self, extension):                     # returns the of data of the language where 'extension' belongs
+		for sublist in self.languages.values():                     # if extension isn't found, returns None
+			if extension in sublist[0]:                             # sublist = [extensionsList]
 				return sublist
-
+		
 		return None
 
-	def getLanguageFromExtension (self, extension):						# returns the language name associated with an extension
+	def getLanguageFromExtension (self, extension):
 		dataList = self.getListFromExtension (extension)
-		for key, value in self.languages.iteritems():					
+		for key, value in self.languages.iteritems():             # returns the language name associated with an extension
 			if value == dataList:
 				return key
 
 		return None
 
-	def incrementLanguageCounter (self, extension):
-		self.incrementLanguageCounter (self.getLanguageFromExtension (extension))
-
 	def incrementLanguageCounter (self, language):
-		self.languages[language][1] += 1
+		if language in self.languagesCounter:			
+			self.languagesCounter[language] += 1
 
-	def updateStats (self, fileName):
+		else:
+			self.languagesCounter.update ({language: 1})
+
+	def updateStats (self, fileName):										# checks the file extension and updates the respective counters 
 		extension = fileName [fileName.rfind('.')+1 :]
 		languageName = self.getLanguageFromExtension (extension)
 	
-		if languageName == None:														# extension wasn't found in .yml
-			if extension not in self.languages['Unknown'][0]:
-				self.languages['Unknown'][0].append (extension)
-
-			self.languages['Unknown'][1] += 1
-			# encontrar o UNKOWN, incrementar o contador e adicionar a extensao	
+		if languageName == None:                                            # extension wasn't found in .yml
+			self.unknownLanguagesCounter += 1
+			if extension not in self.unknownLanguages:
+				self.unknownLanguages.append (extension)
 
 		else:
-				self.incrementLanguageCounter (languageName)
+				self.incrementLanguageCounter (languageName)				#extension was found (recognized) in .yml
 				self.recognizedFilesCounter += 1
 
 		self.totalFilesCounter += 1
@@ -119,14 +110,14 @@ class Polyglot:
 	def runAnalysis (self, fileName):
 		filePath = self.parseFileName (fileName)
 
-		if not os.path.exists (filePath):
+		if not os.path.exists (filePath):									# no file or directory exists with 
 			errMsg = "File \""+fileName+"\" doesn't exist."
 			raise Exception (errMsg)
 
-		if os.path.isfile (filePath):
+		if os.path.isfile (filePath):										# if the fileName is associated with a file, checks for its extension
 			self.updateStats (fileName)
 
-		elif os.path.isdir (filePath):
+		elif os.path.isdir (filePath):										# if the fileName is associated with a directory, runs the check for each file within the directory (recursively)
 			contentList = os.listdir (filePath)
 			for file in contentList:
 				self.runAnalysis (file)
