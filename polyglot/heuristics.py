70f1ca6fa49d6464bc413cd00708d7bb035f7f78
Miguel Pires <miguelpires94@gmail.com>
2014-04-29 23:49:37 +0100

    Changed heuristics and added a yaml for syntax strings
    The checks are now performed in a more generic and scalable way.
    Also added a syntax.yml with string for Perl and Prolog

--- polyglot/heuristics.py
+++ polyglot/heuristics.py
@@ -1,24 +1,28 @@
+import polyglot
+import yaml
 
 class Heuristics:
 
 
 	""" Heuristics """
+
+
 	def __init__(self, path, possibleLanguages):
-		self.filePath = path
-		self.possibleLanguages = possibleLanguages
+		self.syntaxFile = polyglot.Polyglot.tryOpenFile ('syntax.yml')
+		self.syntaxBits = yaml.safe_load (self.syntaxFile)
 		self.disambiguate(path, possibleLanguages)
 
+	def disambiguate(self, path, possibleLanguages):
+		#checks the syntax strings of every possible language until it finds a match
 
-	def disambiguate(self, path, languages):
-		if "Perl" and "Prolog"  in languages:
-			return self.disambiguate_pl (path);
-
-
-	def disambiguate_pl (self, path):
 		with open (path) as sourceCode:
-			if ":-" in sourceCode.read():
-				return "Prolog"
+			for lang in possibleLanguages:
+				if lang not in self.syntaxBits.keys():
+					continue		#there are syntax strings for that language
 
-			sourceCode.seek (0)			#re-reads from the beginning of the file
-			if  "#!/usr/local/bin/perl" in sourceCode.read():
-				return "Perl"
+				else:
+					for string in self.syntaxBits [lang]:
+						if string in sourceCode.read():
+							return lang
+						
+						sourceCode.seek (0)			#re-reads from the beginning of the file
